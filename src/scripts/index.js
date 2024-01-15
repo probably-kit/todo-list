c1e3ec43fb97610c2ce64f5ed8c2c89c0d8b967b
probably-kit <probably.kit@gmail.com>
2024-01-15 19:00:16 +0100

    working prototype

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,3 +4,38 @@ import './../style.css';
 
 
 console.log("webpack is working");
+
+document.addEventListener('DOMContentLoaded', function () {
+    function addTask() {
+        const taskInput = document.getElementById('taskInput');
+        const taskList = document.getElementById('taskList');
+    
+        if (taskInput.value !== '') {
+            const li = document.createElement('li');
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            li.appendChild(checkbox);
+    
+            const taskText = document.createElement('span');
+            taskText.textContent = taskInput.value;
+            li.appendChild(taskText);
+    
+            taskList.appendChild(li);
+            taskInput.value = '';
+    
+            checkbox.addEventListener('change', function () {
+                if (checkbox.checked) {
+                    taskText.style.textDecoration = 'line-through';
+                } else {
+                    taskText.style.textDecoration = 'none';
+                }
+            });
+        }
+    }
+
+    const addTaskButton = document.getElementById('addTask');
+    addTaskButton.addEventListener('click', addTask);
+    
+});
+
+
--- src/scripts/todo.js
+++
@@ -1,5 +0,0 @@
-const Todo = (title, description, dueDate, priority) => {
-    return { title, description, dueDate, priority };
-  };
-  
-  export default Todo;
